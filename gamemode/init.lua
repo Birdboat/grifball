e2154074dedf0a4b21a8a430b830225832944ccf
Birdboat <birdserver1@gmail.com>
2015-10-25 17:00:43 -0500

    Commit

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,3 +3,18 @@ AddCSLuaFile("cl_init.lua")
 
 include("shared.lua")
 
+function GM:PlayerSpawn(ply)
+	ply:SetupHands(ply)
+end
+
+function GM:PlayerSetHandsModel( ply, ent )
+
+	local simplemodel = player_manager.TranslateToPlayerModelName( ply:GetModel() )
+	local info = player_manager.TranslatePlayerHands( simplemodel )
+	if ( info ) then
+		ent:SetModel( info.model )
+		ent:SetSkin( info.skin )
+		ent:SetBodyGroups( info.body )
+	end
+
+end
